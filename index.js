8ca108fc9ca034503cb02e5a4a347599b19b9c20
bhatiamunal <bhatiamunal8987@gmail.com>
2021-09-05 12:00:32 -0700

    new design

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { constants } = require('buffer');
+const bodyParser = require("body-parser")
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app);
@@ -6,38 +6,53 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000
 //user =[{id :"",name:"",data:[{id:"",message:"",time:""}]}]
 let user=[
-        {username:"munal",password:"123",status:"offline"},
-        {username:"sahil",password:"123",status:"offline"}
+        {id:1,username:"munal",password:"123",status:"offline"},
+        {id:2,username:"sahil",password:"123",status:"offline"}
         ];
-
+        app.use(bodyParser.urlencoded({
+            extended:true
+        }));
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
-
+function loginCheck(username,password){
+    // let username = username;
+    // let password = password;
+   
+    return user.filter(ele=>{
+        return username== ele.username && password==ele.password
+    })
+}
 app.use(express.static(__dirname + '/public'))
 app.use(express.json())
 app.set('view engine','ejs')
+let message = "Please Enter Username and Password"
 app.get('/', (req, res) => {
    //res.sendFile(__dirname + '/index.html')
-    res.render("login")
+    res.render("login",{msg:message})
   // res.redirect('demo') 
 })
 
-app.get('/loginCheck', (req, res) => {
+app.post('/loginCheck', (req, res) => {
 
     //res.redirect('demo') 
-     console.log(req.query)
-    let username = req.query.username;
-    let password = req.query.password;
-    user.forEach(ele=>{
-        if(username== ele.username && password==ele.password){
-            ele.status ="online";
-            res.render('demo',{current_user:ele.username});
-        } 
-    })
+    //  console.log(req.body)
+    //  if(loginCheck(req.body.username,req.body.password)){
+    //     res.render('demo',{current_user:ele.username});
+    //  }
+    //  else {
+    //     res.redirect('/')
+    //  }
    
    // res.send(`Username: ${username} Password: ${password}`);
-   
+   let activeUser  = loginCheck(req.body.username,req.body.password);
+   if(activeUser.length>0){
+        res.render('demo',{current_user:activeUser[0].username});
+   }
+   else {
+    message = "Username or Password is wrong" 
+    res.redirect('/')
+   }
  }) 
 
 app.get('/demo', (req, res) => {
@@ -49,14 +64,14 @@ app.get('/demo', (req, res) => {
 const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
-    console.log('Connected...')
-    console.log('socketId'+socket.id)
+    // console.log('Connected...')
+    // console.log('socketId'+socket.id)
     //let socketid = socket.id
     //user.push({id:socket.id,message:[]})
     //io.to(socketid).emit('message', 'for your eyes only');
     socket.on('new-user',name=>{
-        user.some(ele=>{ele})
-        user[socket.id] = name
+        // user.some(ele=>{ele})
+        // user[socket.id] = name
         console.log(user)
         //socket.broadcast.emit("user-joined",name)
         // console.log("new user", name)
@@ -76,4 +91,10 @@ io.on('connection', (socket) => {
        console.log(msg)
         socket.broadcast.emit('recived',{message:msg,name:user[socket.id]})
     })
+    socket.on("disconnect", msg => {
+        
+        socket.broadcast.emit('leave',user[socket.id]);
+        delete user[socket.id];
+        // console.log(socket.id); // undefined
+    });
 })
